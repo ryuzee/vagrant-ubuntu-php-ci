7de2dd47b8f602cce98ac91302591c68cab73525
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-04-11 06:40:59 +0900

    change to latest version

--- site-cookbooks/base/recipes/phing.rb
+++ site-cookbooks/base/recipes/phing.rb
@@ -7,9 +7,9 @@
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
 
-# https://github.com/sebastianbergmann/phpcpd
+# http://www.phing.info/trac/wiki/Users/Download
 remote_file "/usr/local/bin/phing" do
-  source "http://www.phing.info/get/phing-2.6.1.phar"
+  source "http://www.phing.info/get/phing-latest.phar"
   action :create_if_missing
   owner "root"
   group "root"

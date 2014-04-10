7a3ffb4e902c0193be1a0765db51aeb730b38fb8
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-04-11 06:51:44 +0900

    change to pear command

--- site-cookbooks/base/recipes/phing.rb
+++ site-cookbooks/base/recipes/phing.rb
@@ -8,10 +8,10 @@
 # http://opensource.org/licenses/mit-license.php
 
 # http://www.phing.info/trac/wiki/Users/Download
-remote_file "/usr/local/bin/phing" do
-  source "http://www.phing.info/get/phing-latest.phar"
-  action :create_if_missing
-  owner "root"
-  group "root"
-  mode 0755
+execute "pear channel-discover pear.phing.info" do
+  action :run
+end
+
+execute "pear upgrade --alldeps phing/phing" do
+  action :run
 end

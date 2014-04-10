b0964853cfa6d6427236d8b20dd7ce8d62771136
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-04-10 19:37:05 +0900

    add PHP_CodeSniffer

---
+++ site-cookbooks/base/recipes/php_codesniffer.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: base
+# Recipe:: php_codesniffer
+#
+# Copyright 2014, Ryutaro YOSHIBA
+#
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+
+execute "pear upgrade --alldeps PHP_CodeSniffer" do
+  action :run
+end

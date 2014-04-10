d75e28eb9b2d7a95c7db4b3e968b4bb1a6d8fc02
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-04-10 10:56:56 +0900

    add phpcpd

---
+++ site-cookbooks/base/recipes/phpcpd.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: base
+# Recipe:: phpcpd
+#
+# Copyright 2014, Ryutaro YOSHIBA
+#
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+
+# https://github.com/sebastianbergmann/phpcpd
+remote_file "/usr/local/bin/phpcpd" do
+  source "https://phar.phpunit.de/phpcpd.phar"
+  action :create_if_missing
+  owner "root"
+  group "root"
+  mode 0755
+end

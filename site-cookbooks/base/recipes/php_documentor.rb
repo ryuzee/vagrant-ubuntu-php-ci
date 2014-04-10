40187e7383e53394da7d5c976493cfa6e573dff1
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-04-10 19:47:56 +0900

    add phpDocumentor

---
+++ site-cookbooks/base/recipes/php_documentor.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: base
+# Recipe:: php_documentor
+#
+# Copyright 2014, Ryutaro YOSHIBA
+#
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+
+remote_file "/usr/local/bin/phpdoc" do
+  source "http://phpdoc.org/phpDocumentor.phar"
+  action :create_if_missing
+  owner "root"
+  group "root"
+  mode 0755
+end

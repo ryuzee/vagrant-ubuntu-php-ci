5ac0ad347efc96f11908da92a3a3fc4a75010c93
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-04-13 16:40:32 +0900

    add new option to change document root

--- site-cookbooks/phpenv/attributes/default.rb
+++ site-cookbooks/phpenv/attributes/default.rb
@@ -1,4 +1,5 @@
 default['nginx']['docroot']['owner'] = 'root'
 default['nginx']['docroot']['group'] = 'root'
+default['nginx']['docroot']['path'] = "/usr/share/nginx/html"
 default['mysql']['root_password'] = 'password'
 default['php_library']['install_method'] = 'pear'
